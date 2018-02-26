87b9eec11437474aa374c8dd65a656a68d30bbf8
casual_ji <casual_ji@hotmail.com>
2018-02-26 23:26:10 +0800

    修改message接口

---
+++ src/main/java/cn/jihangyu/glowworm/message/entity/MessageElement.java
@@ -0,0 +1,16 @@
+package cn.jihangyu.glowworm.message.entity;
+
+import lombok.Data;
+
+@Data
+public class MessageElement {
+  private Message message;
+
+    private String fromUnickbname;
+
+    private String fromUcity;
+
+    private String bookname;
+
+    private String bookimg;
+}
--- src/main/java/cn/jihangyu/glowworm/message/service/MessageService.java
+++ src/main/java/cn/jihangyu/glowworm/message/service/MessageService.java
@@ -1,17 +1,18 @@
 package cn.jihangyu.glowworm.message.service;
 
 import cn.jihangyu.glowworm.message.entity.Message;
+import cn.jihangyu.glowworm.message.entity.MessageElement;
 
 import java.util.List;
 
 public interface MessageService {
     String addMessage(Message message);
 
-    List<Message> getRequestMessage(String to_uid);
+    List<MessageElement> getRequestMessage(String to_uid);
 
     Message updateMessage(Message message);
 
-    List<Message> getReplyMessage(String from_uid);
+    List<MessageElement> getReplyMessage(String from_uid);
 
     Message getMessageById(Integer id);
 }
